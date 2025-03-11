ddedbb81b3c24f80db5dac9c9006f30d8f80cd17
Kard9876 <dark9876king@gmail.com>
2025-03-11 15:41:34 +0000

    Tensorflow embedding

--- Code/utils/dataset.py
+++ Code/utils/dataset.py
@@ -60,8 +60,7 @@ class Dataset:
         y_validation = y_validation.drop(id_column, inplace=False, axis=1)
         y_validation = y_validation.to_numpy().reshape(y_validation.shape[0], )
 
-        self._label_encoder_fitted = True
-        y_validation = self._label_encoder.fit_transform(y_validation)
+        y_validation = self._label_encoder.transform(y_validation)
 
         return X_validation, y_validation
 
@@ -86,8 +85,7 @@ class Dataset:
             y_test = y_test.drop(id_column, inplace=False, axis=1)
             y_test = y_test.to_numpy().reshape(y_test.shape[0], )
 
-            self._label_encoder_fitted = True
-            y_test = self._label_encoder.fit_transform(y_test)
+            y_test = self._label_encoder.transform(y_test)
 
         return X_test, y_test, ids
 
@@ -95,6 +93,70 @@ class Dataset:
         X_train, y_train = self.get_train_dataset(fit_column, target, sep=sep, rem_punctuation=rem_punctuation, id_column=id_column)
         X_validation, y_validation = self.get_validation_dataset(fit_column, target, sep=sep, rem_punctuation=rem_punctuation, id_column=id_column)
         X_test, y_test, ids = self.get_test_dataset(fit_column, target, sep=sep, rem_punctuation=rem_punctuation, id_column=id_column)
+
+        return X_train, y_train, X_validation, y_validation, X_test, y_test, ids
+
+    def get_train_dataset_embedding(self, fit_column, target, sep=';', rem_punctuation=False, id_column='ID'):
+        X_train = pd.read_csv(self._train_input, sep=sep)
+        X_train = X_train.drop(id_column, inplace=False, axis=1)
+
+        if rem_punctuation:
+            X_train[fit_column] = X_train[fit_column].apply(remove_ponctuation)
+
+        y_train = pd.read_csv(self._train_output, sep=sep)
+        y_train = y_train.drop(id_column, inplace=False, axis=1)
+        y_train = y_train.to_numpy().reshape(y_train.shape[0], )
+
+        self._label_encoder_fitted = True
+        y_train = self._label_encoder.fit_transform(y_train)
+
+        return X_train, y_train
+
+    def get_validation_dataset_embedding(self, fit_column, target, sep=';', rem_punctuation=False, id_column='ID'):
+        if not self._label_encoder_fitted:
+            raise Exception("Please obtain train dataset first")
+
+        X_validation = pd.read_csv(self._validation_input, sep=sep)
+        X_validation = X_validation.drop(id_column, inplace=False, axis=1)
+
+        if rem_punctuation:
+            X_validation[fit_column] = X_validation[fit_column].apply(remove_ponctuation)
+
+        y_validation = pd.read_csv(self._validation_output, sep=sep)
+        y_validation = y_validation.drop(id_column, inplace=False, axis=1)
+        y_validation = y_validation.to_numpy().reshape(y_validation.shape[0], )
+
+        y_validation = self._label_encoder.transform(y_validation)
+
+        return X_validation, y_validation
+
+    def get_test_dataset_embedding(self, fit_column, target, sep=';', rem_punctuation=False, id_column='ID'):
+        if not self._label_encoder_fitted:
+            raise Exception("Please obtain train dataset first")
+
+        X_test = pd.read_csv(self._test_input, sep=sep)
+        ids = X_test[id_column]
+        X_test = X_test.drop(id_column, inplace=False, axis=1)
+
+        if rem_punctuation:
+            X_test[fit_column] = X_test[fit_column].apply(remove_ponctuation)
+
+        y_test = None
+
+        if self._test_output is not None:
+            y_test = pd.read_csv(self._test_output, sep=sep)
+
+            y_test = y_test.drop(id_column, inplace=False, axis=1)
+            y_test = y_test.to_numpy().reshape(y_test.shape[0], )
+
+            y_test = self._label_encoder.transform(y_test)
+
+        return X_test, y_test, ids
+
+    def get_datasets_embedding(self, fit_column, target, sep=';', rem_punctuation=False, id_column='ID'):
+        X_train, y_train = self.get_train_dataset_embedding(fit_column, target, sep=sep, rem_punctuation=rem_punctuation, id_column=id_column)
+        X_validation, y_validation = self.get_validation_dataset_embedding(fit_column, target, sep=sep, rem_punctuation=rem_punctuation, id_column=id_column)
+        X_test, y_test, ids = self.get_test_dataset_embedding(fit_column, target, sep=sep, rem_punctuation=rem_punctuation, id_column=id_column)
 
         return X_train, y_train, X_validation, y_validation, X_test, y_test, ids
 
